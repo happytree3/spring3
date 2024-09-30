83c9b322463d37ad87aec6c1afcecc4d1e708565
yoosone93 <yoosone93@naver.com>
2024-09-30 12:40:47 +0900

    test

---
+++ src/main/java/kr/bit/controller/HomeController.java
@@ -0,0 +1,36 @@
+package kr.bit.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Handles requests for the application home page.dsdasd  dddd
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name. test test 33
+	 */
+	@RequestMapping("/")
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home"; //home.jsp
+	}
+	
+}
